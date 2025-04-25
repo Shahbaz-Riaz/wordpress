7b344a751935db19e6c5015ddd16efbad60188af
shahbaz <muhammad.shahbazt20@gmail.com>
2025-04-25 15:03:59 +0500

    sample project

---
+++ app/public/wp-content/themes/my-theme/header.php
@@ -0,0 +1,13 @@
+<nav class="header">
+    <div class="logo">Education</div>
+    <ul class="nav-links">
+      <!-- <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Contact</a></li>
+       -->
+       <?php 
+      wp_nav_menu(array('theme_location'=>'primary-menu','menu_class'=>'nav'))
+      ?>
+    </ul>
+  </nav>
